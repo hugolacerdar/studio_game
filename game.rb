2fe2b3272447398609f9431f2dd9c9cd39bf62c1
Hugo <hugolacerdar@gmail.com>
2022-02-09 17:46:32 -0300

    refactor: add game_turn module

--- game.rb
+++ game.rb
@@ -1,5 +1,5 @@
 require_relative 'player'
-require_relative 'die'
+require_relative 'game_turn'
 
 class Game 
 
@@ -27,16 +27,7 @@ class Game
         end
 
         @players.each do |player|
-            die = Die.new
-
-            case die.roll
-            when 1..2
-                player.blam
-            when 3..4
-                puts "#{player.name} was ignored."
-            else
-                player.w00t
-            end
+            GameTurn.take_turn(player)
             puts player
         end
     end
