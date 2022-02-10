679cc74132c4c4e2da26431903211fcb46881ba3
Hugo <hugolacerdar@gmail.com>
2022-02-10 12:33:19 -0300

    test: add sort test

--- game_spec.rb
+++ game_spec.rb
@@ -34,4 +34,18 @@ describe Game do
             expect(@player.health).to eq(@initial_health - 10 * 2)
         end
     end
+
+    context "in a collection of players" do
+        before do
+          @player1 = Player.new("moe", 100)
+          @player2 = Player.new("larry", 200)
+          @player3 = Player.new("curly", 300)
+      
+          @players = [@player1, @player2, @player3]
+        end
+      
+        it "is sorted by decreasing score" do
+          @players.sort.should == [@player3, @player2, @player1]
+        end
+      end
 end
