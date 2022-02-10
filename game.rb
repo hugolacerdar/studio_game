7ab8bbfc9fec4b56a5dc9656ad3f4332817d9984
Hugo <hugolacerdar@gmail.com>
2022-02-10 12:41:29 -0300

    chore: add game stats

--- game.rb
+++ game.rb
@@ -20,16 +20,45 @@ class Game
         end
     end
 
-    def play
-        puts "There are #{@players.size} players in #{@title}: "
+    def play(rounds = 1)
+        puts "\nThere are #{@players.size} players in #{@title}: "
         @players.each do |player|
           puts player
         end
 
-        @players.each do |player|
-            GameTurn.take_turn(player)
-            puts player
+        1.upto(rounds) do |round|
+            puts @players.empty? ? "No players to play" : "\nRound #{round}"
+
+            @players.each do |player|
+                GameTurn.take_turn(player)
+                puts player
+            end
         end
+
+        print_stats
+    end
+
+    def print_stats
+        if(@players.empty?) 
+            return
+        end
+
+        puts "\n#{@title} Statistics:"
+
+        strong_players, wimpy_players = @players.partition {|player| player.strong?}
+        
+        puts "\n#{strong_players.size} strong players:"
+        strong_players.each {|player| print_name_and_health(player)} 
+        
+        puts "\n#{wimpy_players.size} wimpy players:"
+        wimpy_players.each {|player| print_name_and_health(player)} 
+
+        puts "\n#{@title} High Scores:"
+        @players.sort.each {|player| puts player.score_string}
+    end
+
+    def print_name_and_health(player) 
+        puts "#{player.name} (#{player.health})"
     end
 end
 
