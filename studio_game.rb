a10d1f9314bb1abf69fdfb3da1b569ebdd9aa385
Hugo <hugolacerdar@gmail.com>
2022-02-10 20:36:17 -0300

    chore: update example

--- studio_game.rb
+++ studio_game.rb
@@ -3,6 +3,7 @@ require_relative 'game'
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
+player4 = Player.new("dorly", 200)
 
 battledroids = Game.new("Battledroids of Ooo")
 
@@ -11,4 +12,8 @@ battledroids.add_players(player1)
 battledroids.add_players([player3, player2])
 battledroids.play(5)
 
-
+doom = Game.new("Doom of Ooo")
+doom.add_players([player1, player2, player3, player4])
+doom.play(10) do
+    doom.total_gold_collected >= 20000
+end
