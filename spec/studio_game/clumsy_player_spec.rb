65634cb3eb3fe23e224520e92ef7ea0d9655d986
Hugo <hugolacerdar@gmail.com>
2022-02-12 18:46:32 -0300

    refactor: spec directory

---
+++ spec/studio_game/clumsy_player_spec.rb
@@ -0,0 +1,52 @@
+require 'studio_game/clumsy_player'
+require 'studio_game/treasure_trove'
+
+module StudioGame
+  describe ClumsyPlayer do
+    before do
+      $stdout = StringIO.new
+      @player = ClumsyPlayer.new("klutz")
+    end
+
+    it "only gets half the point value for each treasure" do
+      expect(@player.total_gold).to eq(0)
+
+      hammer = Treasure.new(:hammer, 50)
+      @player.store_treasure(hammer)
+      @player.store_treasure(hammer)
+      @player.store_treasure(hammer)
+
+      expect(@player.total_gold).to eq(75)
+
+      crowbar = Treasure.new(:crowbar, 400)
+      @player.store_treasure(crowbar)
+
+      expect(@player.total_gold).to eq(275)
+
+      yielded = []
+      @player.each_treasure do |treasure|
+        yielded << treasure
+      end
+
+      expect(yielded).to eq([Treasure.new(:"damaged hammer", 75), Treasure.new(:"damaged crowbar", 200)])
+    end
+
+    context "with a w00t booster" do
+      before do
+        @initial_health = 100
+        @w00t_booster = 5
+        @player = ClumsyPlayer.new("klutz", @initial_health, @w00t_booster)
+      end
+    
+      it "has a w00t booster" do
+        expect(@player.w00t_booster).to eq(5)
+      end
+    
+      it "gets w00t_booster number of w00ts when w00ted" do
+        @player.w00t
+    
+        expect(@player.health).to eq(@initial_health + (15 * @w00t_booster))
+      end
+    end  
+  end
+end
---
+++ spec/studio_game/game_spec.rb
@@ -0,0 +1,80 @@
+require 'studio_game/game'
+
+module StudioGame
+  describe Game do
+      before do
+          @game = Game.new('corn wars')
+          $stdout = StringIO.new
+      end
+
+      context 'one player match' do
+          before do
+              @initial_health = 100
+              @player = Player.new('kert', @initial_health)
+              @game.add_players(@player)
+          end
+
+          it "w00ts the player if a high number is rolled" do 
+              allow_any_instance_of(Die).to receive(:roll).and_return(5)
+              @game.play(3)
+
+              expect(@player.health).to eq(@initial_health + 15 * 3)
+          end
+
+          it "ignores the player if a medium number is rolled" do 
+              allow_any_instance_of(Die).to receive(:roll).and_return(3)
+              @game.play
+
+              expect(@player.health).to eq(@initial_health)
+          end
+
+          it "blams the player if a low number is rolled" do 
+              allow_any_instance_of(Die).to receive(:roll).and_return(2)
+              @game.play(2)
+
+              expect(@player.health).to eq(@initial_health - 10 * 2)
+          end
+      end
+
+      context "in a collection of players" do
+          before do
+            @player1 = Player.new("moe", 100)
+            @player2 = Player.new("larry", 200)
+            @player3 = Player.new("curly", 300)
+        
+            @players = [@player1, @player2, @player3]
+          end
+        
+          it "is sorted by decreasing score" do
+            expect(@players.sort).to eq([@player3, @player2, @player1])
+          end
+        end
+
+        it "assigns a treasure for total gold during a player's turn" do
+          game = Game.new("Knuckleheads")
+          player = Player.new("moe")
+        
+          game.add_players(player)
+        
+          game.play(1)
+        
+          expect(player.total_gold).not_to be_zero      
+        end
+
+        it "computes total gold collected as the sum of all player gold" do
+          game = Game.new("Knuckleheads")
+        
+          player1 = Player.new("moe")
+          player2 = Player.new("larry")
+        
+          game.add_players(player1)
+          game.add_players(player2)
+        
+          player1.store_treasure(Treasure.new(:hammer, 50))
+          player1.store_treasure(Treasure.new(:hammer, 50))
+          player2.store_treasure(Treasure.new(:crowbar, 400))
+        
+          expect(game.total_gold_collected).to eq(500)
+        end
+  end
+end
