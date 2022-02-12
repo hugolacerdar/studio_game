bb67b8acf6784d2e9663becbcd3d967215365f66
Hugo <hugolacerdar@gmail.com>
2022-02-12 15:04:43 -0300

    test: w00t booster test

--- clumsy_player_spec.rb
+++ clumsy_player_spec.rb
@@ -30,4 +30,21 @@ describe ClumsyPlayer do
     expect(yielded).to eq([Treasure.new(:"damaged hammer", 75), Treasure.new(:"damaged crowbar", 200)])
   end
 
+  context "with a w00t booster" do
+    before do
+      @initial_health = 100
+      @w00t_booster = 5
+      @player = ClumsyPlayer.new("klutz", @initial_health, @w00t_booster)
+    end
+  
+    it "has a w00t booster" do
+      expect(@player.w00t_booster).to eq(5)
+    end
+  
+    it "gets w00t_booster number of w00ts when w00ted" do
+      @player.w00t
+  
+      expect(@player.health).to eq(@initial_health + (15 * @w00t_booster))
+    end
+  end  
 end
