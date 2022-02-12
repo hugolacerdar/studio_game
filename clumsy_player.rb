6f725ba74d2c6fbbb020af9f80ccde9561f09d7a
Hugo <hugolacerdar@gmail.com>
2022-02-12 15:05:00 -0300

    chore: add w00t booster

--- clumsy_player.rb
+++ clumsy_player.rb
@@ -1,6 +1,17 @@
 require_relative "player"
 
 class ClumsyPlayer < Player
+    attr_reader :w00t_booster
+
+    def initialize(name, health = 100, w00t_booster = 2)
+        super(name, health)
+        @w00t_booster = w00t_booster
+    end
+
+    def w00t
+        @w00t_booster.times { super }
+    end
+
     def store_treasure(treasure)
         super(damage_treasure(treasure))
     end
